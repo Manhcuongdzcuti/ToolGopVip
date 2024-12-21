fa4cb9f4f0befb965d4ddf04ecdf47cf5b40493c
Manhcuongdzcuti <caothithuhien123mx@gmail.com>
2024-12-21 18:37:24 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,2 @@
 print('\033[1;32mHi Ae. Tool Thì Không Còn Hoạt Động Nữa Cảm Ơn Ae Đã Tin Tưởng Ủng Hộ Mình Trong Thời Gian Vừa Qua, Có Lẽ Mình Vẫn Chưa Thực Sự Có Duyên Với Bộ Môn Này. Mong Rằng Thời Gian Sắp Tới, Mình Không Chắc Chắn Thế Nhưng Mà Mình Sẽ Cố Gắng Và Nổ Lực Hơn Và Sẽ Mở Lại Sever Cho Ae Cày Tiền Nhé Còn Hiện Tại Thì Mình Chưa Thực Sự Thấy Ổn Sau Khoảng Tg Code!')
+exit()
